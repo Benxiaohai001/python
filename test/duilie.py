db85a6ea27cb96c37af48c4634cf8cd7e8ec4220
bca <1447333108@qq.com>
2022-11-24 17:45:30 +0800

    add

--- test/duilie.py
+++ test/duilie.py
@@ -1,25 +1,26 @@
-class Node():
-    def __init__(self, data) -> None:
-        self.val = data
-        self.next = None
+def search_word_num(string):
+    end = 0
+    first = 0
+    string_list = []
+    for i in string:
+        if i == ' ' or end == len(string)-1:
+            if end == len(string)-1:
+                string_list.append(string[first:])
+                first = end + 1
+            else:
+                string_list.append(string[first:end])
+                first = end + 1
+        end = end + 1
 
-class Solution:
+    # print(string_list)
+    # string_list = string.strip(" ")
+    string_dic = {}
+    for i in string_list:
+        if i in string_dic.keys():
+            string_dic[i] = string_dic[i] + 1
+        else:
+            string_dic[i] = 1
+    print(string_dic)
 
-    def __init__(self) -> None:
-        self.head = None
-        self.tail = None
+search_word_num("hello world hello")
     
-    def push(self, data):
-        node = Node(data)
-        pre = self.head
-        while pre:
-            p
-
-
-    def pull():
-
-
-if __name__ == '__main__':
-    s = Solution()
-    s.work([1, 2, 3])
-
